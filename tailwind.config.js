8daa9c636f7bf62ddf12a34371aa2545f6c58e07
Diego Arce <dnarce@gmail.com>
2022-04-24 15:40:56 -0300

    actualizo tailwind y daisyui agrego tema personalizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,31 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        neonBlue: '#5D73F0',
-        platinum: '#E6E6E6',
-        yellowMunsell: '#EBC909',
-        oldGold: '#CFB004',
-      }
+        neonBlue: "#5D73F0",
+        platinum: "#E6E6E6",
+        yellowMunsell: "#EBC909",
+        oldGold: "#CFB004",
+      },
     },
   },
+  daisyui: {
+    themes: [
+      {
+        workbox: {
+          primary: "#5d73f0",
+          secondary: "#ebc909",
+          accent: "#fcd34d",
+          neutral: "#cfb004",
+          "base-100": "#e6e6e6",
+          info: "#3458EA",
+          success: "#108e73",
+          warning: "#efda71",
+          error: "#ef4e66",
+        },
+      },
+    ],
+  },
   plugins: [require("daisyui")],
-}
+};
